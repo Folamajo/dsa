cf62aaab3959c4f773aa7dad5a4aa3ca49a7b1c3
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-05-03 05:30:17 +0100

    finished count bits

--- Blind75/binary/count_bits.py
+++ Blind75/binary/count_bits.py
@@ -6,21 +6,29 @@ class Solution(object):
       :type n: int
       :rtype: List[int]
       """
-      #Brute Force
+      #Brute Force — O(n log n)
+      # ans = [0] * (n + 1)
+      # binary_values = []
+      # for i in range(n+1):
+      #    binary = bin(i)[2:]
+      #    binary_values.append(binary)
+      
+      
+      # for index, value in enumerate(binary_values):
+      #    count = 0
+      #    for num in value:       
+      #       if num == '1':
+      #          count += 1
+      #       ans[index] = count
+      # return ans
+
+
+      #O(n)
       ans = [0] * (n + 1)
-      binary_values = []
-      for i in range(n+1):
-         binary = bin(i)[2:]
-         binary_values.append(binary)
-      
-      
-      for index, value in enumerate(binary_values):
-         count = 0
-         for num in value:       
-            if num == '1':
-               count += 1
-            ans[index] = count
+      for i in range(1, n+1):
+         ans[i] = ans[i >> 1] + (i & 1)
       return ans
 
+
 solution = Solution()
-print(solution.countBits(2))
+print(solution.countBits(5))
