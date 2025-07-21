d4e6824687d4de5f2ce1023824231dfd829b036c
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-07-21 06:38:24 +0100

    Finished can_construct tabulation

---
+++ dp_course/tabulation/can_construct_tab.py
@@ -0,0 +1,26 @@
+def can_construct_tab(target, word_bank):
+   table = [False] * (len(target) + 1)
+   table[0] = True
+
+   for i in range(len(target)):
+      if table[i] == True:
+         for word in word_bank:
+            if target[i : i + len(word)] == word:
+               table[i+len(word)] = True
+
+   return table[len(target)]
+   #    if table[i] == False:
+   #       for value in word_bank:
+   #          if target[i] in value:
+   #             table[i] = True
+
+   # if False in table:
+   #    return False
+   # return True
+
+
+print(can_construct_tab('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'])) #True
+print(can_construct_tab("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))  # False
+print(can_construct_tab("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]))  # True  
+print(can_construct_tab("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"]))  # False  
+print(can_construct_tab("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"])) #True
