5a0f417ac9e6156552564a66a3bf753c4ab862f1
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-07-29 15:52:31 +0100

    started count_construct_table

--- dp_course/tabulation/count_construct_tab.py
+++ dp_course/tabulation/count_construct_tab.py
@@ -1,9 +1,14 @@
 # write a function countConsruct(target, wordank) that accepts a target string and an array of strings.
 
-# The functionshould return the number of ways the `target` can be constructred by concatenanting elements of 
+# The function should return the number of ways the `target` can be constructred by concatenanting elements of 
 # the `wordBank` array.
 
 # You may reuse elements of `wordbank` as many times as needed
 
 def count_construct_tab(target, wordbank):
    table = [0] * (len(target) + 1)
+   table[0] = 1
+
+   for i in range(len(table)):
+      if table[i] > 0:
+         
