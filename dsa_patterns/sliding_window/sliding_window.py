aa831708c17befaa6970361ee01ef181ed742a48
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-08-05 08:51:20 +0100

    Started sliding window pattern

---
+++ dsa_patterns/sliding_window/sliding_window.py
@@ -0,0 +1,4 @@
+def sliding_window(int_list:list, window_size:int) -> int:
+   if len(int_list) == 0 or window_size > len(int_list):
+      raise ValueError("Window size must be less than length of integer list and integer list must not be empty")
+    
