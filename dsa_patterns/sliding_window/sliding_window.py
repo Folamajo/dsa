7c64e8d3ffdb98d7ffde77e5582697b3d827fca1
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-08-08 08:02:38 +0100

    Working on sliding window

--- dsa_patterns/sliding_window/sliding_window.py
+++ dsa_patterns/sliding_window/sliding_window.py
@@ -1,4 +1,9 @@
+
+
 def sliding_window(int_list:list, window_size:int) -> int:
    if len(int_list) == 0 or window_size > len(int_list):
       raise ValueError("Window size must be less than length of integer list and integer list must not be empty")
-    
+
+   current_sum = sum(int_list[0:window_size])
+   max_sum = current_sum
+   
