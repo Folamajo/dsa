b76d9f09ac05feb2149a154959191be5c82117f2
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-08-09 20:13:54 +0100

    Finished sliding window

--- dsa_patterns/sliding_window/sliding_window.py
+++ dsa_patterns/sliding_window/sliding_window.py
@@ -6,4 +6,11 @@ def sliding_window(int_list:list, window_size:int) -> int:
 
    current_sum = sum(int_list[0:window_size])
    max_sum = current_sum
+
+   for i in range(window_size, len(int_list)) :
+      current_sum = current_sum - int_list[i - window_size] + int_list[i]
+      if current_sum > max_sum:
+         max_sum = current_sum
    
+   return max_sum
+print(sliding_window([2, 1, 5, 1, 3, 2], 3))
