1746daca361080ced2aae64c6e49854aaf13954e
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-08-18 07:33:56 +0100

    Finished longest substring with slidiing window

--- dsa_patterns/sliding_window/longest_substring.py
+++ dsa_patterns/sliding_window/longest_substring.py
@@ -1,49 +1,51 @@
 #BRUTE FORCE
 
-def length_of_longest_substring(string):
+# def length_of_longest_substring(string):
 
-   if len(string) == 1 :
-      return 1
+#    if len(string) == 1 :
+#       return 1
 
-   max_length = 0 
+#    max_length = 0 
    
-   for i in range(len(string)):
-      current_substring = []
-      current_substring.append(string[i])
-      for j in range(i+1, len(string)):
-         if string[j] in current_substring:
-            max_length = max(max_length, len(current_substring))
-            break
-         else:
-            current_substring.append(string[j])
-            max_length = max(max_length, len(current_substring))
+#    for i in range(len(string)):
+#       current_substring = []
+#       current_substring.append(string[i])
+#       for j in range(i+1, len(string)):
+#          if string[j] in current_substring:
+#             max_length = max(max_length, len(current_substring))
+#             break
+#          else:
+#             current_substring.append(string[j])
+#             max_length = max(max_length, len(current_substring))
 
-   return max_length
+#    return max_length
 
-def length_of_longest_substring(string):
+
+def length_of_longest_substring(string): #n is the length of the string
    left = 0
    right = 0
    max_length = 0
    window_chars = set()
 
-   for right in range(len(string)):
+   for right in range(len(string)): #Loop will execute n times 
+      while string[right] in window_chars: #Left can move n times 
+         window_chars.remove(string[left])
+         left += 1
+      
+      window_chars.add(string[right])
+      window_size = right - left + 1 
+      if max_length < window_size:
+         max_length = window_size
 
-# def length_of_longest_substring(string):
+   return max_length
 
-#    if len(string) == 1:
-#       return 1 
+
+#TIME COMPLEXITY 
+   #Algorithm uses a sliding window with two pointers. 
+   # Right pointer expands the window with n times and the left pointer only advances at most n times so this gives O(n + n)
+
+#SPACE COMPLEXITY
    
-#    max_length = 0
-#    start = 0
-#    stop = 1
-#    longest_string = []
-#    for i in range(len(string)):
-      
-      
-
-      # print(string[start], string[stop])
-      # return
-
 print(length_of_longest_substring("abcabcbb"))
 print(length_of_longest_substring("bbbbb"))
 print(length_of_longest_substring("pwwkew"))
