2eb491e6b87ad27f68beb6887ce3bfe0263a5641
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-08-21 08:11:38 +0100

    minimum window substring

--- dsa_patterns/sliding_window/minimum_window_substring.py
+++ dsa_patterns/sliding_window/minimum_window_substring.py
@@ -15,9 +15,27 @@ def min_window(s:str, t:str)-> str:
    right = 0
    min_window = ""
 
+   #Checks if window is valid
    need = len(t_count)
    have = 0
 
-   
+   best_window= float("inf")
+   best_start = -1
+   bext_end = -1 
+
+   for right in range(len(s)):
+      curr_char = s[right]
+      if not curr_char in window_count:
+         window_count[curr_char] = 1
+      else:
+         window_count[curr_char] += 1
+
+      if curr_char in t_count:
+         if window_count[curr_char] == t_count[curr_char]:
+            have += 1
+
+      while have == need:
+         
+
 
    return 
