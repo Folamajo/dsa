9b4f04b4c49ee13e657c2c3423b193ec1b6358d8
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-09-23 16:44:18 +0100

    max product

--- dsa_patterns/pattern_foundations/kadane_algorithm/maximum_product_subarray.py
+++ dsa_patterns/pattern_foundations/kadane_algorithm/maximum_product_subarray.py
@@ -3,7 +3,7 @@ def max_product(nums: list):
    
    curr_max = nums[0]
    curr_min = nums[0]
-   max_product = float('-inf')
+   max_product = nums[0]
 
    for i in range(len(nums)):
       curr_max = max(nums[i], (nums[i] * curr_max))
