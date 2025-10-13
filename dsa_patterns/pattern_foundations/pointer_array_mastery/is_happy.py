f3e7abe09daef18f34356a2f2b729746cf5b3ee2
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-10-13 06:44:58 +0100

    Started is_happy

--- dsa_patterns/pattern_foundations/pointer_array_mastery/is_happy.py
+++ dsa_patterns/pattern_foundations/pointer_array_mastery/is_happy.py
@@ -1,4 +1,26 @@
+def is_happy(input:int)->bool:
+   
+   def get_next(num):
+      sum = 0 
+      for digit in str(num):
+         sum += (int(digit) ** 2)
+      return sum
 
+   fast = slow = input
+
+   while True:
+      slow = get_next(slow)
+      fast = get_next(get_next(fast))
+
+      if slow == fast:
+         return False
+      elif fast == 1 or slow == 1:
+         return True
+
+
+if __name__ == "__main__":
+   input = 1
+   print(is_happy(input))
 
 
 # QUESTION : 202. Happy Number
@@ -15,5 +37,14 @@
    # else we return false 
    # We can detect the repitition using Floyd's cycle detection 
    # Create a function that accepts a number and get the sum of the squares of its digits 
-   # Brute force would mean we store everything in a set and check if we've seen it before. 
+   # # Brute force would mean we store everything in a set and check if we've seen it before. 
 
+   # We have our input we create a function that calculates the squares of our number we set our slow and fast pointers to the input we run our function for slow and fast if either is equal to 1 we know we have a happy number we return true if fast === slow we know we have a cycle we return false
+
+# PSEUDOCODE
+   # 1. We define a function that takes a value and returns the sum of the square digits 
+   # 2. We set two variables fast and slow to equal our input 
+   # 3. We run a while loop that chacks if fast and slow are not equal to 1 
+   # 4. Inside the loop we run out function on slow and set the value to slow i.e slow = f(slow) 
+   # 5. We do the same for fast but we want two steps so we say f(f(fast) 
+   # 6. we check if fast and slow are equal we return false 7. We return true
