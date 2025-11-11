3a964ea7f53fdbc5dfda109dfaa25d7d13cc23de
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-11-11 07:17:14 +0000

    Did min stack but with O(n)

---
+++ dsa_patterns/pointer_array_mastery/stack_queue/min_stack.py
@@ -0,0 +1,19 @@
+class MinStack:
+   def __init__(self):
+      self.stack = []
+
+   def push(self, val:int)-> None:
+      self.stack.append(val)
+      return
+
+   def pop(self)->int:
+      if len(self.stack) > 0:
+         return self.stack.pop()
+      
+   def top(self)-> int:
+      if len(self.stack) > 0:
+         return self.stack[len(self.stack) - 1]
+      
+   def getMin(self) -> int:
+      if len(self.stack) > 0:
+         return min(self.stack)
