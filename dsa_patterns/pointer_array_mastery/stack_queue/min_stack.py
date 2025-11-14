5131c78d821d42cb7a64f9f8298e7c06f1d4d393
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-11-14 13:29:44 +0000

    Fixed min_stack with O(1) time complixity

--- dsa_patterns/pointer_array_mastery/stack_queue/min_stack.py
+++ dsa_patterns/pointer_array_mastery/stack_queue/min_stack.py
@@ -1,16 +1,21 @@
 class MinStack:
    def __init__(self):
       self.stack = []
-      self.min = {}
+      self.min = []
 
 
    def push(self, val:int)-> None:
       self.stack.append(val)
       
+      if len(self.min) == 0 or val < self.min[-1]:
+         self.min.append(val)
+      else:
+         self.min.append(self.min[-1])
       return
 
    def pop(self)->int:
       if len(self.stack) > 0:
+         self.min.pop()
          return self.stack.pop()
       
    def top(self)-> int:
