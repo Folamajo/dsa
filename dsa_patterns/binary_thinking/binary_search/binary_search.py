98ec2688e3e7f5073da8ff671b6f34c310212ce5
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-11-20 16:59:46 +0000

    binary search implemented

--- dsa_patterns/binary_thinking/binary_search/binary_search.py
+++ dsa_patterns/binary_thinking/binary_search/binary_search.py
@@ -1,3 +1,19 @@
-def binary_search(input:list):
+def binary_search(input:list, target:int):
    # initialise varibales 
-   
+   left = 0
+   right = len(input) - 1
+
+   while left <= right:
+      middle = left + int((right - left ) / 2)
+      if input[middle] == target : 
+         return middle
+      elif target > input[middle]:
+         left = middle + 1
+      else:
+         right = middle - 1
+   return -1 
+
+if __name__ == "__main__":
+   input = [-1,0,3,5,9,12]
+   target = 9 
+   print(binary_search(input, target))
