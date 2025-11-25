5fb767b6066e61a065dd554499b785e3c271b091
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-11-25 08:43:38 +0000

    Finished search rotated

--- dsa_patterns/binary_thinking/binary_search/search_rotated.py
+++ dsa_patterns/binary_thinking/binary_search/search_rotated.py
@@ -8,12 +8,21 @@ def search_rotated(nums: list[int], target: int) -> int:
       if nums[midpoint] == target: 
          return midpoint
       
-      elif nums[left] > nums[midpoint]: 
-         #We know right side is sorted 
-         if target > nums[midpoint] and target <= nums[right]
+      elif nums[left] <= nums[midpoint]: #checks if left side is sorted 
+         if target >= nums[left] and target < nums[midpoint]:
+            right = midpoint - 1
+         else :
             left = midpoint + 1
+      else:
+         if target > nums[midpoint] and target <= nums[right]:
+            left = midpoint + 1
+         else:
+            right = midpoint - 1
 
-      else: #left side is sorted
-
-
+   return -1 
 # In this scenario the rotated side always has one sorted half and one unsorted half 
+
+if __name__ == "__main__":
+   nums = [4,5,6,7,0,1,2]
+   target = 0
+   print(search_rotated(nums, target))
