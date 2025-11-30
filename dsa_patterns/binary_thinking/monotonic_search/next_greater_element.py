da57807e93e24afbcd6db25c02efec95d3a242e4
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-11-30 04:16:49 +0000

    started next greater element

---
+++ dsa_patterns/binary_thinking/monotonic_search/next_greater_element.py
@@ -0,0 +1,29 @@
+
+def nextGreaterElement(nums1:list[int], nums2: list[int]) -> list[int]:
+   stack = []
+   dict = {}
+
+   for i in range( len(nums2)):
+      if len(stack) == 0 :
+         stack.append(nums2[i])
+      else: 
+         if nums2[i] > stack[-1]:
+            stack.pop()
+            stack.append(nums2[i])
+
+   return stack
+
+if __name__ == "__main__":
+   nums1 = [4,1,2]
+   nums2 = [1,3,4,2]
+   print(nextGreaterElement(nums1, nums2))
+#Brute force 
+# For each number in nums1:
+# Find where that number appears in nums2
+# From that index, scan to the right
+# Look for the first number that is greater
+# If you find one → that’s the next greater
+# If not → return -1
+# This is 100% correct for the brute force way.
+
+#Stack is LIFO
