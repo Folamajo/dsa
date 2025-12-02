0f883c7e30e2d9b71c1b189dac63f99fc340d7b5
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-12-02 11:13:57 +0000

    2nd implementation

--- dsa_patterns/binary_thinking/monotonic_search/next_greater_element.py
+++ dsa_patterns/binary_thinking/monotonic_search/next_greater_element.py
@@ -4,29 +4,26 @@ def nextGreaterElement(nums1:list[int], nums2: list[int]) -> list[int]:
    dict = {}
    result = []
    
-   for i in range( len(nums2)):
-      if len(stack) == 0 :
+   for i in range(len(nums2)):
+      if len(stack) == 0 or nums2[i] < stack[-1]:
          stack.append(nums2[i])
-      else: 
-         if nums2[i] > stack[-1]:
+
+      else:
+         while stack and nums2[i] > stack[-1]:
             remove = stack.pop()
             dict[remove] = nums2[i]
-            stack.append(nums2[i])
-         elif nums2[i] < stack[-1] :
-             dict[stack[-1]] = -1
-             stack.append(nums2[i])
-
+   
+      stack.append(nums2[i])
       dict[nums2[-1]] = -1
-
+   
    for i in range(len(nums1)):
       if nums1[i] in dict:
          result.append(dict[nums1[i]])
-
-   return result
+   return dict
 
 if __name__ == "__main__":
-   nums1 = [4,1,2]
-   nums2 = [1,3,4,2]
+   nums1 = [1,3,5,2,4]
+   nums2 = [6,5,4,3,2,1,7]
    print(nextGreaterElement(nums1, nums2))
 
 # Brute force 
@@ -38,4 +35,23 @@ if __name__ == "__main__":
 # If not → return -1
 # This is 100% correct for the brute force way.
 
-#Stack is LIFO
+#Stack is LIFO   
+
+
+# # for i in range( len(nums2)):
+   #    if len(stack) == 0 :
+   #       stack.append(nums2[i])
+   #    else: 
+   #       if nums2[i] > stack[-1]:
+   #          remove = stack.pop()
+   #          dict[remove] = nums2[i]
+   #          stack.append(nums2[i])
+   #       elif nums2[i] < stack[-1] :
+   #           dict[stack[-1]] = -1
+   #           stack.append(nums2[i])
+
+   #    dict[nums2[-1]] = -1
+
+   # for i in range(len(nums1)):
+   #    if nums1[i] in dict:
+   #       result.append(dict[nums1[i]])
