b728d6df4408163d0040475a24caf7ddfdff780e
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-12-24 09:18:12 +0000

    Finished largest rectangle area

--- dsa_patterns/binary_thinking/monotonic_search/largest_rectangle_area.py
+++ dsa_patterns/binary_thinking/monotonic_search/largest_rectangle_area.py
@@ -14,7 +14,14 @@ def largestRectangleArea(heights:list[int])->int:
             left_boundary = -1
          else: left_boundary = stack[-1]
 
-         width = current_height
+         width = i - left_boundary -1 
+         area = width * top_height
+         if area > max_area:
+            max_area = area
+      
+      stack.append(i)
+
+   return max_area
 
 #CONCEPT
 # The stack stores indices in increasing height order
@@ -27,3 +34,14 @@ def largestRectangleArea(heights:list[int])->int:
 # Area = height x width
 # Track the maximum area as you pop 
 # After the scan ends, we treat the end of the array as the right boundary and pop everything left in the stack the same way. 
+
+
+# TIME COMPLEXITY 
+# a single index can be push into the stack at least once and it can be popped once
+# So total stack operations is 2n 
+# Even though theres a while loop inside a for loop the total work done by the while loop across the entire run is linear not per iteration
+# Time complexity: O(n)
+
+# SPACE COMPLEXITY 
+# In the worst case the stack can grow to the size of (n)
+# Space complexity: O(n)
