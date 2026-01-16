34de8f5d64dc6ed6cd9b54439306b6664e30e48c
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2026-01-16 07:58:01 +0000

    finished subsets

--- dsa_patterns/recursion_core/backtracking/subsets.py
+++ dsa_patterns/recursion_core/backtracking/subsets.py
@@ -3,7 +3,19 @@ def subsets(nums:list[int])->list[list[int]]:
    def helper(subset:list[int], start: int):
       subsets.append(list(subset))
       for i in range(start, len(nums)):
+         subset.append(nums[i])
+         helper(subset, i + 1)
+         subset.pop()
+
+   helper([], 0)
+   return subsets
 
 if __name__ == "__main__":
    nums = [1,2,3]
    print(subsets(nums))
+
+
+#  Total number of subsets for n elements is 2^n 
+#  Each one will take n work to copy/store so time complexity is (n x 2n)
+#  
+
