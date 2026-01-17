570d98895cc895ed71ee7da62601e255ede8b101
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2026-01-17 08:17:17 +0000

    Started combination_sum

---
+++ dsa_patterns/recursion_core/backtracking/combination_sum.py
@@ -0,0 +1,8 @@
+def combination_sum(candidates: list[int], target:int) -> list[list[int]]:
+   output = []
+   def helper(path: list[int], start : int, target: int):
+      path_sum = sum(path)
+      if path_sum == target:
+
+      if path_sum> target: 
+
