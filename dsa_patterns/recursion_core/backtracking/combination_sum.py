a2b3f20acc5db5b04fd22a4c98d9d9323e3d994d
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2026-01-19 05:28:50 +0000

    Implemented combination sum

--- dsa_patterns/recursion_core/backtracking/combination_sum.py
+++ dsa_patterns/recursion_core/backtracking/combination_sum.py
@@ -1,8 +1,28 @@
 def combination_sum(candidates: list[int], target:int) -> list[list[int]]:
    output = []
-   def helper(path: list[int], start : int, target: int):
-      path_sum = sum(path)
-      if path_sum == target:
+   path = []
+   start = 0
+   current_total = 0
+   def helper(path: list[int], start : int, target: int, current_total: int):
+      if current_total == target:
+         output.append(list(path))
+         return 
+      if current_total > target: 
+         return
+      
+      for i in range(start, len(candidates)):
+         path.append(candidates[i])
+         current_total += candidates[i]
+         helper(path, i, target, current_total)
+         remove = path.pop()
+         current_total -= remove
+   
+   helper(path, start, target, current_total)
+   return output
 
-      if path_sum> target: 
 
+
+if __name__ == "__main__": 
+   nums = [2,3,6,7]
+   target = 7 
+   print(combination_sum(nums, target))
