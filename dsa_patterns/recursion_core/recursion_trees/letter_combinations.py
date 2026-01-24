836a668f278a8320ceccba281e2a5504d82264fa
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2026-01-24 09:54:54 +0000

    Defined concept

---
+++ dsa_patterns/recursion_core/recursion_trees/letter_combinations.py
@@ -0,0 +1,12 @@
+def letter_combinations(digits: str ) -> list[str]: 
+
+
+
+if __name__ == "__main__":
+
+# CONCEPT
+#  In letter cmbinations, each level of the recursion tree corresponds to one digit in the input string
+#  The index of the current digit and the partially buil string represent the state at each recursion node
+#  We stop and record a result when the length of the built string equals the numbder of digits 
+#  The number of letters mapped to the current digit will determine how many branches the nodes will have
+#  No prunning is needed because every partial string can become valid.
