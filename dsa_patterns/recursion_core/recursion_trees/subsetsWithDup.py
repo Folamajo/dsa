d9409c1c9538669aba27f32c0cc944023a81ffdf
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2026-01-29 08:56:45 +0000

    Added time and space complexity

--- dsa_patterns/recursion_core/recursion_trees/subsetsWithDup.py
+++ dsa_patterns/recursion_core/recursion_trees/subsetsWithDup.py
@@ -18,3 +18,12 @@ def subsetsWithDup(nums: list[int])->list[list[int]]:
 if __name__ == "__main__":
    nums = [1,2,2]
    print(subsetsWithDup(nums))
+
+
+   # TIME COMPLEXITY
+   # If input array has length n the maximum number of unique subsets that can be generated is O(2 ^ n)
+   # If each subset we generate can be up to length n and there are up to 2^n the most accurate time complexity is O(n * 2^n)
+
+   # SPACE COMPLEXITY
+   # Maximum space used but the recursion stak and path excluding the output list is O (n)
+   # When we include the required to store all subsets the total complexity becomes O(n * 2^n)
