6e49a8578c26ba5d4394d24cc775e287ed5c4f35
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2026-02-19 23:45:21 +0000

    Added time and space complexity

--- dsa_patterns/recursion_core/depth_first_search/hasPathSum.py
+++ dsa_patterns/recursion_core/depth_first_search/hasPathSum.py
@@ -20,6 +20,14 @@ if __name__ == "__main__":
    root = TreeNode(5)
    root.left = TreeNode(4)
    root.right = TreeNode(8)
+   root.left.left = TreeNode(11)
+   root.left.left.left = TreeNode(7)
+   root.left.left.right = TreeNode(2)
+   root.right.left = TreeNode(13)
+   root.right.right = TreeNode(4)
+   root.right.right.right = TreeNode(1)
+
+
    print(hasPathSum(root, targetSum=22))
 
 # CONCEPT 
@@ -28,3 +36,13 @@ if __name__ == "__main__":
 # We handle the running sum by passing the remaining target down recursively
 # When we reach a leaf node we return True if the RemainingSum - node.val === 0 
 # When branching recursively we need to check either side by return dfs(left) and dfs(right)
+
+# TIME COMPLEXITY
+# If the binary tree had n nodes the time complexity is 
+# Worst case we might visit every node once
+# We do constant work per node 
+# So TIME = O(n)
+
+# SPACE COMPLEXITY 
+# The space complexity in terms of tree height h 
+# O(h)
