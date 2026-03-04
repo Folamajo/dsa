03ae3e65a2ca333548c4462a19cc96fd22fb974b
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2026-03-04 10:42:08 +0000

    implemented rottng oranges

--- dsa_patterns/breadth_trees/BFS/rottingOranges.py
+++ dsa_patterns/breadth_trees/BFS/rottingOranges.py
@@ -15,6 +15,7 @@ def rottingOranges(grid:list[list[int]]) -> int:
       for col in range(cols):
          curr_orange = grid[row][col]
          if curr_orange == 2:
+
             queue.append([row, col])
          
          elif curr_orange == 1: 
@@ -27,9 +28,22 @@ def rottingOranges(grid:list[list[int]]) -> int:
       for _ in range(level_size):
          rotten_orange = queue.popleft()
          row, col = rotten_orange
+         directions = [(-1, 0), (1,0), (0, -1), (0, 1)]
+         for (dr, dc) in directions:
+            new_row = row + dr
+            new_col = col + dc
+            if new_row >= 0 and new_row < rows and new_col >= 0 and new_col < cols:
+               if grid[new_row][new_col] == 1:
+                  grid[new_row][new_col] = 2
+                  queue.append([new_row, new_col])
+                  fresh -= 1
+      minutes += 1
+      
+   return minutes if fresh == 0 else -1
 
-         
-
+if __name__ == "__main__":
+   grid = [[2,1,1], [1,1,0], [0,1,1]]
+   print(rottingOranges(grid))
 # PATTERN RECOGNITION
 # Rotting oranges is best classified as Multi source BFS
 # Because rotting spreads simultaneously from all rotten oranges
